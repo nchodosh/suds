585774512b4d33b8070c98d9e4094a301f499434
Nate Chodosh <nchodosh@gmail.com>
2024-03-06 19:56:45 -0500

    try again

--- suds/suds_model.py
+++ suds/suds_model.py
@@ -702,9 +702,9 @@ class SUDSModel(Model):
                                                   1 / self.config.entropy_increase_max_iters)) \
                                           ** step
         
-    def get_outputs_for_camera(self, camera: Cameras, obb_box: Optional[OrientedBox] = None) -> Dict[str, torch.Tensor]:
-        outputs = super().get_outputs_for_camera(camera, obb_box)
-        outputs['rays'] = camera.generate_rays(camera_indices=0, keep_shape=True, obb_box=obb_box)
+    def get_outputs_for_camera_ray_bundle(self, camera_ray_bundle: RayBundle, render_options: Optional[Dict[str, any]] = None) -> Dict[str, torch.Tensor]:
+        outputs = super().get_outputs_for_camera_ray_bundle(camera_ray_bundle, render_options)
+        outputs['ray_directions'] = camera_ray_bundle.directions
         return outputs
     
     def get_outputs(self, ray_bundle: RayBundle, render_options: Optional[Dict[str, Any]] = None) -> Dict[
@@ -1346,17 +1346,17 @@ class SUDSModel(Model):
             'lpips': float(lpips),
         }
 
-        ground_truth_z = batch["depth_image"].to(self.device)
-        if not self.config.is_euclidean_depth:
-            ground_truth_depth = ground_truth_z * outputs["directions_norm"]
-        else:
-            ground_truth_depth = ground_truth_z
-
-        depth_mask = ground_truth_depth > 0
-        target_depth = ground_truth_depth[depth_mask]
         
-        ground_truth_xyz = (outputs['rays'].directions[depth_mask.squeeze(), :] * target_depth[..., None]).float()
-        predicted_xyz = (outputs['rays'].directions * outputs['expected_depth']).reshape(-1, 3)
+        ground_truth_z = batch["depth_image"].to(self.device)
+        
+        z_mask = ground_truth_z > 0
+        target_z = ground_truth_z[z_mask]
+
+        ray_dirs = outputs['ray_directions']
+        img_plane_points = ray_dirs / ray_dirs[..., 2]
+        
+        ground_truth_xyz = (img_plane_points[z_mask.squeeze(), :] * target_z[..., None]).float()
+        predicted_xyz = (img_plane_points.directions * depth).reshape(-1, 3)
 
         sq_dists, _, _ = knn_points(ground_truth_xyz[None], predicted_xyz[None], K = 1)
         dists = sq_dists.reshape(-1).sqrt()
